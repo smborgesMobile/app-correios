a84181056f86008113ab98fbf4a741d6a9eb9015
Sérgio Borges <sergioborges.ciandt@natura.net>
2024-08-13 23:18:51 -0300

    Update primary color

--- app/src/main/java/br/com/smdevelopment/rastreamentocorreios/ui/theme/Color.kt
+++ app/src/main/java/br/com/smdevelopment/rastreamentocorreios/ui/theme/Color.kt
@@ -4,6 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val primary500 = Color(0xff3d85c6)
-val primary700 = Color(0xff3d85c6)
+val primary700 = Color(0xFF1A80E5)
 val disabledButton = Color(0xFFEBECF0)
 val Teal200 = Color(0xFF03DAC5)
