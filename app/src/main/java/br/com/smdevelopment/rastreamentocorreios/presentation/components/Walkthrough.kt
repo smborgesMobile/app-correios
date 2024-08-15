18d47542a4113052e184c8476f2e9d71f6843930
Sérgio Borges <sergioborges.ciandt@natura.net>
2024-08-15 16:51:59 -0300

    upgrade AGP + Gradle

---
+++ app/src/main/java/br/com/smdevelopment/rastreamentocorreios/presentation/components/Walkthrough.kt
@@ -0,0 +1,124 @@
+package br.com.smdevelopment.rastreamentocorreios.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.smdevelopment.rastreamentocorreios.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WalkThrough(
+    modifier: Modifier = Modifier
+) {
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+
+    // Get the screen height
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = { showBottomSheet = true }
+        ) {
+            Text("Display partial bottom sheet")
+        }
+
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .navigationBarsPadding(),
+                sheetState = sheetState,
+                onDismissRequest = { showBottomSheet = false },
+                shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally, // Center items horizontally
+                    verticalArrangement = Arrangement.Top // Keep items at the top
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.image_quadro),
+                        contentDescription = null,
+                        modifier = Modifier.padding(16.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        text = "Boas vindas ao novo Crie seus Posts!",
+                        modifier = Modifier.padding(horizontal = 24.dp),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold)
+                    )
+
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    Text(
+                        text = "Você pode buscar por nome, marca, categoria ou código. Para excluir, selecione o produto e toque na lixeira na área superior da tela. ",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(horizontal = 24.dp),
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Row(
+                        modifier = Modifier
+                            .padding(bottom = 42.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        NewButton("Anterior", {})
+
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        NewButton("Próximo", {})
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WalkThroughPreview() {
+    MaterialTheme {
+        WalkThrough()
+    }
+}
