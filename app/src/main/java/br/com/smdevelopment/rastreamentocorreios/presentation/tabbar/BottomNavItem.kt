26d7b8f8c3f013552ab3b20551f99972fdc132fa
Sérgio Borges <sergioborges.ciandt@natura.net>
2024-08-18 10:52:08 -0300

    Create new icons for bottom sheet

--- app/src/main/java/br/com/smdevelopment/rastreamentocorreios/presentation/tabbar/BottomNavItem.kt
+++ app/src/main/java/br/com/smdevelopment/rastreamentocorreios/presentation/tabbar/BottomNavItem.kt
@@ -3,10 +3,10 @@ package br.com.smdevelopment.rastreamentocorreios.presentation.tabbar
 import br.com.smdevelopment.rastreamentocorreios.R
 
 sealed class BottomNavItem(val title: String, val icon: Int, val route: String) {
-    data object Delivered : BottomNavItem(DELIVERED, R.drawable.icon_car, DELIVERED.lowercase())
-    data object Home : BottomNavItem(HOME, R.drawable.icon_home, HOME.lowercase())
-    data object Pending : BottomNavItem(IN_PROGRESS, R.drawable.icon_running, IN_PROGRESS.lowercase())
-    data object Price : BottomNavItem(PRICE, R.drawable.icon_running, PRICE.lowercase())
+    data object Delivered : BottomNavItem(DELIVERED, R.drawable.delivered_bs_icon, DELIVERED.lowercase())
+    data object Home : BottomNavItem(HOME, R.drawable.home_bs_icon, HOME.lowercase())
+    data object Pending : BottomNavItem(IN_PROGRESS, R.drawable.in_progress_bs_icon, IN_PROGRESS.lowercase())
+    data object Price : BottomNavItem(PRICE, R.drawable.price_bs_icon, PRICE.lowercase())
 
     private companion object {
         const val HOME = "Home"
